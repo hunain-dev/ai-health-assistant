2dc960f28b1367bfcf301942f6e2f9b5138b0e9c
hunii ali <huniiali1122@gmail.com>
2025-11-06 23:05:25 +0500

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-[18vh] w-full border-2 border-black'>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home/Section/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../../Components/Header'
+
+const Home = () => {
+  return (
+    <div className='h-[100vh] w-full bg-[#F9F9F9] px-3'>
+        <h1 className='Canopee font-[500] text-center py-6 text-4xl'> medihub.</h1>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
