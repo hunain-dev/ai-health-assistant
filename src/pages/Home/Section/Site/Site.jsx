db4d6ad54fe295e6eac4ac93595e1e7b68e89847
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-11-07 23:46:48 +0500

    updated

--- src/pages/Home/Section/Site/Site.jsx
+++ src/pages/Home/Section/Site/Site.jsx
@@ -1,13 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineLogout } from "react-icons/ai";
 import { CiChat1, CiHome } from "react-icons/ci";
-import { FaMicrophone } from "react-icons/fa";
+import { FaFile, FaImage, FaMicrophone } from "react-icons/fa";
 import { GoPlus } from "react-icons/go";
 import { IoIosSend } from "react-icons/io";
 import { Link } from "react-router-dom";
 import Loader from "../../../../Components/Loader";
+import { TiWorld } from "react-icons/ti";
 
 const Site = () => {
+  const [prompt, setPrompt] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [languageoepn, setlanguageoepn] = useState(false);
+  const [selectedLang, setSelectedLang] = useState("English");
+
+  // all languages
+  const languages = [
+    { name: "English", flag: "🇬🇧" },
+    { name: "Urdu", flag: "🇵🇰" },
+    { name: "Hindi", flag: "🇮🇳" },
+    { name: "Arabic", flag: "🇸🇦" },
+    { name: "French", flag: "🇫🇷" },
+    { name: "Spanish", flag: "🇪🇸" },
+    { name: "German", flag: "🇩🇪" },
+    { name: "Italian", flag: "🇮🇹" },
+    { name: "Chinese (Simplified)", flag: "🇨🇳" },
+    { name: "Japanese", flag: "🇯🇵" },
+    { name: "Korean", flag: "🇰🇷" },
+    { name: "Turkish", flag: "🇹🇷" },
+    { name: "Russian", flag: "🇷🇺" },
+    { name: "Portuguese", flag: "🇵🇹" },
+    { name: "Bengali", flag: "🇧🇩" },
+    { name: "Persian", flag: "🇮🇷" },
+    { name: "Malay", flag: "🇲🇾" },
+    { name: "Indonesian", flag: "🇮🇩" },
+    { name: "Thai", flag: "🇹🇭" },
+    { name: "Greek", flag: "🇬🇷" },
+    { name: "Dutch", flag: "🇳🇱" },
+    { name: "Swedish", flag: "🇸🇪" },
+    { name: "Polish", flag: "🇵🇱" },
+    { name: "Vietnamese", flag: "🇻🇳" },
+    { name: "Tamil", flag: "🇮🇳" },
+    { name: "Pashto", flag: "🇦🇫" },
+  ];
+
+  const handleLanguageChange = (lang) => {
+    setSelectedLang(lang.name);
+    setOpen(false);
+  };
+
+  //   file open
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  //   voice option
+  const handleVoiceClick = () => {
+    setShowPopup(true);
+
+    setTimeout(() => {
+      setShowPopup(false);
+    }, 4000);
+  };
+
+  //   ai promit answer
+  const [messages, setMessages] = useState([
+    {
+      sender: "ai",
+      text: "Hello! I'm your AI Health Assistant. How are you feeling today?",
+    },
+  ]);
+
+  const handleSend = () => {
+    if (!prompt.trim()) return;
+
+    const userMsg = { sender: "user", text: prompt };
+    const aiReply = {
+      sender: "ai",
+      text: "It seems you might have a mild issue. Drink water, rest well, and if it continues, please contact a doctor.",
+    };
+
+    setMessages([...messages, userMsg, aiReply]);
+    setPrompt("");
+  };
+
+  //   ai promit answer
+
   return (
     <div className="h-full w-full">
       <div className="h-full mt-7 w-full grid grid-cols-[1fr_10fr]  p-4">
@@ -16,38 +95,106 @@ const Site = () => {
             <div className="flex items-center justify-center flex-col gap-6">
               {" "}
               <Link to="/">
-                <CiHome    className="text-white cursor-pointer text-3xl" />
+                <CiHome className="text-white cursor-pointer text-3xl" />
               </Link>
-              <CiChat1     className="text-white cursor-pointer text-3xl" />
+              <CiChat1 className="text-white cursor-pointer text-3xl" />
+              <TiWorld
+                onClick={() => setlanguageoepn(!languageoepn)}
+                className="text-white cursor-pointer text-3xl"
+              />
+              {languageoepn && (
+                <div className="absolute bottom-50 left-27 bg-white shadow-xl rounded-xl border border-gray-200 p-2 w-60 max-h-65 overflow-y-scroll borde z-50 animate-fadeIn">
+                  {languages.map((lang, index) => (
+                    <button
+                      key={index}
+                      onClick={() => handleLanguageChange(lang)}
+                      className={`actay w-full flex items-center gap-2 text-left px-3 py-2 rounded-md text-sm hover:bg-gray-100 ${
+                        lang.name === selectedLang ? "bg-gray-100 " : ""
+                      }`}
+                    >
+                      <span>{lang.flag}</span> {lang.name}
+                    </button>
+                  ))}
+                </div>
+              )}
             </div>
 
             <AiOutlineLogout className="text-white cursor-pointer  text-2xl" />
           </div>
         </div>
-      
-<div className="h-full w-full overflow-hidden  rounded-3xl">
-<Loader  />
 
-  <div className="h-full relative p-3">
-    <div className="py-4 flex items-center justify-between px-4 rounded-full w-[95%] border border-[#e2e0e0] absolute bottom-8 left-1/2 -translate-x-1/2 bg-[#FFFFFF] shadow-md">
-    <div className="flex items-center gap-3">
-        <GoPlus   className="text-3xl text-gray-700  cursor-pointer hover:text-black transition" />
-      </div>
-            
-            <input
-        type="text"
-        placeholder="Enter a prompt..."
-        className="flex-1 outline-none border-none text-black text-[16px] bg-transparent px-2"
-      />
+        <div className="h-full w-full overflow-hidden  rounded-3xl">
+          <Loader />
 
-      {/* Icons Section */}
-      <div className="flex items-center gap-3">
-        <FaMicrophone className="text-xl text-gray-700 cursor-pointer hover:text-black transition" />
-        <IoIosSend className="text-2xl text-gray-700 cursor-pointer hover:text-black transition" />
-      </div>
-    </div>
-  </div>
-</div>
+          <div className="h-full relative p-3">
+            <div className="flex-1 overflow-y-auto py-5 space-y-3">
+              {messages.map((msg, index) => (
+                <div
+                  key={index}
+                  className={`flex ${
+                    msg.sender === "user" ? "justify-end" : "justify-start"
+                  }`}
+                >
+                  <div
+                    className={`max-w-[70%] px-4 py-3 rounded-4xl text-sm ${
+                      msg.sender === "user"
+                        ? "border-1 border-gray-700 , text-black"
+                        : "bg-gray-200 text-black"
+                    }`}
+                  >
+                    {msg.text}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className="py-4 flex items-center  justify-between px-4 rounded-full min-w-[95%] border border-[#e2e0e0] absolute bottom-8 left-1/2 -translate-x-1/2 bg-[#FFFFFF] shadow-md">
+              <div className="flex items-center gap-3">
+                <GoPlus
+                  onClick={handleClick}
+                  className="text-3xl text-gray-700  cursor-pointer hover:text-black transition"
+                />
+
+                {/* Popup Menu */}
+                {open && (
+                  <div className="absolute py-7 bottom-12 left-0 bg-white shadow-xl rounded-xl border border-gray-200 px-6 flex flex-col gap-5 z-50 animate-fadeIn">
+                    <button className="actay font-[500] flex items-center gap-2 hover:bg-gray-100 px-2 py-1 rounded-md transition  text-gray-900">
+                      <FaImage className="text-black" /> Add Photo
+                    </button>
+                    <button className="actay flex items-center gap-2 hover:bg-gray-100 px-2 py-1 rounded-md transition  text-gray-900">
+                      <FaFile className="text-black" /> Upload File
+                    </button>
+                  </div>
+                )}
+              </div>
+
+              <input
+                onChange={(e) => setPrompt(e.target.value)}
+                type="text"
+                placeholder="Enter a prompt..."
+                className="flex-1 outline-none border-none text-black text-[16px] bg-transparent px-2"
+              />
+
+              {/* Icons Section */}
+              <div className="flex items-center gap-3">
+                <FaMicrophone
+                  onClick={handleVoiceClick}
+                  className="text-xl text-gray-700 cursor-pointer hover:text-black transition"
+                />
+                {showPopup && (
+                  <div className="actay absolute bottom-30 left-1/2 -translate-x-1/2 bg-[#262D2A] text-white text-[0.9vw] px-6 py-5 rounded-full shadow-md animate-fadeIn">
+                    This feature is not available yet.
+                  </div>
+                )}
+
+                <IoIosSend
+                  onClick={handleSend}
+                  className="text-2xl text-gray-700 cursor-pointer hover:text-black transition"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
